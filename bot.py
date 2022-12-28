fb17b9440fee9b973926863b4ce3468908fd6ceb
veleo1 <115354109+veleo1@users.noreply.github.com>
2022-12-28 16:29:06 +0300

    Update 2
    changed sex variable

--- bot.py
+++ bot.py
@@ -6,19 +6,20 @@ if __name__ == '__main__':
         msg, user_id = vk_bot.get_event()
         if msg == 'начать':
             vk_bot.greeting(user_id)
-            sex = 0
-            while not msg == 'ж' or msg == 'м':
-                vk_bot.write_msg(user_id, 'Введи пол (м - мужской, ж - женский)')
+            sex: int = 0
+            vk_bot.write_msg(user_id, 'Введи пол (1 - женский, 2 - мужской)')
+            while True:
                 msg, user_id = vk_bot.get_event()
-                if msg == 'ж':
-                    sex = 1
-                    break
-                elif msg == 'м':
-                    sex = 2
-                    break
+                try:
+                    if msg == '1' or msg == '2':
+                        sex: int = int(msg)
+                    else:
+                        raise ValueError
+                except ValueError:
+                    vk_bot.write_msg(user_id, 'Введи корректный пол')
                 else:
-                    vk_bot.write_msg(user_id, 'Введите корректный пол')
-            age_from = 0
+                    break
+            age_from: int = 0
             vk_bot.write_msg(user_id, 'Введи минимальный возраст')
             while True:
                 msg, user_id = vk_bot.get_event()
@@ -28,10 +29,10 @@ if __name__ == '__main__':
                     else:
                         age_from = int(msg)
                 except ValueError:
-                    vk_bot.write_msg(user_id, 'Введите корректный возраст')
+                    vk_bot.write_msg(user_id, 'Введи корректный возраст')
                 else:
                     break
-            age_to = 0
+            age_to: int = 0
             vk_bot.write_msg(user_id, 'Введи максимальный возраст')
             while True:
                 msg, user_id = vk_bot.get_event()
@@ -41,18 +42,18 @@ if __name__ == '__main__':
                     else:
                         age_to = int(msg)
                 except ValueError:
-                    vk_bot.write_msg(user_id, 'Введите корректный возраст')
+                    vk_bot.write_msg(user_id, 'Введи корректный возраст')
                 else:
                     break
             vk_bot.write_msg(user_id, 'Введи город')
             msg, user_id = vk_bot.get_event()
-            city = msg
+            city: str = msg
             creating_database()
             vk_bot.search_users(sex, age_from, age_to, city)
             vk_bot.start_searching(user_id, offset)
             insert_data_seen_users(vk_bot.person_id(offset))
             vk_bot.write_msg(user_id, 'Жми на кнопку "Далее", чтобы продолжить поиск.\n'
-                                      'Чтобы поставить лайк фото, напиши номер фото (1, 2 или 3).\n'
+                                      'Чтобы поставить лайк фото, напиши номер фото.\n'
                                       'Чтобы закончить поиск, напиши "пока".')
         elif msg == 'далее':
             for i in range(0, 1000):
@@ -60,15 +61,11 @@ if __name__ == '__main__':
                 vk_bot.start_searching(user_id, offset)
                 insert_data_seen_users(vk_bot.person_id(offset))
                 vk_bot.write_msg(user_id, 'Жми на кнопку "Далее", чтобы продолжить поиск.\n'
-                                      'Чтобы поставить лайк фото, напиши номер фото (1, 2 или 3).\n'
+                                      'Чтобы поставить лайк фото, напиши номер фото.\n'
                                       'Чтобы закончить поиск, напиши "пока".')
                 break
-        elif msg == '1':
-            vk_bot.like_photo1(get_seen_user(), user_id)
-        elif msg == '2':
-            vk_bot.like_photo2(get_seen_user(), user_id)
-        elif msg == '3':
-            vk_bot.like_photo3(get_seen_user(), user_id)
+        elif msg == '1' or msg == '2' or msg == '3':
+            vk_bot.like_photo(get_seen_user(), user_id, msg)
         elif msg == 'пока':
             vk_bot.write_msg(user_id, "Пока((")
         else:

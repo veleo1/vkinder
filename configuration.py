f6a51b79d6b985985de8c854cd59699269ba90a3
veleo1 <115354109+veleo1@users.noreply.github.com>
2022-12-29 19:36:34 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,3 @@
-offset = 0
 V = '5.131'
 group_token = 'vk1.a.wVrXBUaVwpUzJGSTHB6J-GC_8VQ2kGFqyU-FdjzNIqScwVqlrzWoyecXuBUvpT4hGo9Pd2lSs2cmSnp4wVLYRW1UpdwhXrWPRWQ65eBCWFgud0w3ncTfrt47yLL_cdCJyhIaIl3QcFnPFtc1HHfXlw_RQqHNxFO0Q0DPN30-2_1PIO9x4F6Old3AWCZ2GlSHjUZAxRzDhL8HznAprCShYw'
 user_token = input('Token: ')
